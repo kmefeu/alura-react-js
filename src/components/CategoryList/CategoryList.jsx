91a62abecd74ada9f7ebc66a877f521b2c26f7cc
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-12-04 10:16:53 -0300

    Category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "./CategoryList.css";
 class CategoryList extends Component {
-
-  constructor(){
-    super()
-    this.state = {categories:[]}
-    this._newCategories = this._newCategories.bind(this)
+  constructor() {
+    super();
+    this.state = { categories: [] };
+    this._newCategories = this._newCategories.bind(this);
   }
- 
 
   componentDidMont() {
     this.props.categories.subscribe(this._newCategories);
@@ -18,13 +16,12 @@ class CategoryList extends Component {
   }
 
   _newCategories(categories) {
-   this.setState({...this.state,categories})
+    this.setState({ ...this.state, categories });
   }
-
 
   _handleInputEvent(eve) {
     if (eve.key === "Enter") {
-      let categoryValue = eve.target.value
+      let categoryValue = eve.target.value;
       this.props.addCategory(categoryValue);
     }
   }
@@ -32,10 +29,12 @@ class CategoryList extends Component {
   render() {
     return (
       <section className="categoryList">
+      {console.log(this.state.categories)}
         <ul className="ulCategoryList">
           {this.state.categories.map((category, index) => {
+            console.log(category);
             return (
-              <li key={index}className="liCategoryList" >
+              <li key={index} className="liCategoryList">
                 {category}
               </li>
             );
@@ -44,6 +43,7 @@ class CategoryList extends Component {
         <input
           type="text"
           className="inputCategoryList"
+          placeholder="Add Category"
           onKeyUp={this._handleInputEvent.bind(this)}
         />
       </section>
